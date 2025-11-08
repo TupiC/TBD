f2b41cbc5881c5d338ef41d8a8a5358327cd2fb2
Christoph Tupi <christoph.tupi@gmail.com>
2025-11-08 21:57:40 +0100

    feat: add filter route for experiences + fix volumne mount in docker compose

---
+++ apps/cms/src/api/experience/routes/filter.ts
@@ -0,0 +1,12 @@
+export default {
+    routes: [
+        {
+            method: "GET",
+            path: "/experience/filter",
+            handler: "api::experience.filter.filter",
+            config: {
+                auth: false,
+            },
+        },
+    ],
+};
