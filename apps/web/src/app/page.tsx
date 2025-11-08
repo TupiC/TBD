c4ae8972f94f1a5793c498673f8178eedf07542f
Elias Fellinger <elias@MacBook-Pro-von-Elias.local>
2025-11-08 20:40:58 +0100

    chore: shadcn

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
-import { AnimationIcon } from "./components/icons/AnimationIcon";
+import { AnimationIcon } from "@/components/icons/AnimationIcon";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home(): React.JSX.Element {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <AnimationIcon className="w-16 h-16" />
+        <Link href="/personalization">Personalization Page</Link>
       </main>
     </div>
   );
---
+++ apps/web/src/app/personalization/page.tsx
@@ -0,0 +1,9 @@
+const Page = (): React.JSX.Element => {
+  return (
+    <div>
+      <h1>Personalization Page</h1>
+    </div>
+  );
+};
+
+export default Page;
