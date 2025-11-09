fe6ed195fae8c63ae4cf7d7b1c0392c34df25143
Christoph Tupi <christoph.tupi@gmail.com>
2025-11-09 11:43:47 +0100

    chore: redirect to my trips if visits exist

--- apps/web/src/app/onboarding/page.tsx
+++ apps/web/src/app/onboarding/page.tsx
@@ -4,62 +4,71 @@ import ContentPage from "@/components/onboarding/content";
 import RootPage from "@/components/onboarding/root";
 import Body from "@/components/typography/body";
 import Heading from "@/components/typography/heading";
-import { useState } from "react";
+import { useVisitStore } from "@/stores/visit-store";
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Page = (): React.JSX.Element => {
-  const [currentStepIndex, setCurrentStepIndex] = useState(0);
-  const [loading, setLoading] = useState(true);
+    const [currentStepIndex, setCurrentStepIndex] = useState(0);
+    const [loading, setLoading] = useState(true);
+    const visits = useVisitStore((state) => state.visits);
 
-  const next = () => {
-    setCurrentStepIndex(currentStepIndex + 1);
-  };
-  const back = () => {
-    setCurrentStepIndex(currentStepIndex - 1);
-  };
+    const next = () => {
+        setCurrentStepIndex(currentStepIndex + 1);
+    };
+    const back = () => {
+        setCurrentStepIndex(currentStepIndex - 1);
+    };
 
-  const steps: JSX.Element[] = [
-    <RootPage key="root" next={() => next()} />,
-    <ContentPage key="content1" back={() => back()} next={() => next()} />,
-  ];
-  const currentStep = steps[currentStepIndex];
+    const steps: JSX.Element[] = [
+        <RootPage key="root" next={() => next()} />,
+        <ContentPage key="content1" back={() => back()} next={() => next()} />,
+    ];
+    const currentStep = steps[currentStepIndex];
 
-  if (currentStepIndex === 0) {
-    return (
-      <div>
-        {loading && (
-          <div className="h-dvh w-screen flex justify-center items-center">
-            <AnimationIcon
-              duration={0.35}
-              className="w-10 h-10"
-              onComplete={() => {
-                setLoading(false);
-              }}
-            />
-          </div>
-        )}
-        <main className={`${loading ? "invisible" : ""}`}>
-          <div className="h-dvh w-screen">{currentStep}</div>
-          <div className="p-6 py-12 flex flex-col gap-4">
-            <Heading type="h2">
-              Neuer Konfigurator für Salzburg Card Erlebnisse
-            </Heading>
-            <Body>
-              Verpasse keine Erlebnisse und entdecke die besten Angebote! Mit
-              dem neuen Konfigurator kannst du deine Reise ganz nach deinen
-              Wünschen gestalten.
-            </Body>
-            <Body>
-              Sammle Inspiration, sichere dir exklusive Angebote und plane dein
-              Abenteuer in Ruhe. Jeder Klick bringt dich deinem unvergesslichen
-              Erlebnis näher.
-            </Body>
-          </div>
-        </main>
-      </div>
-    );
-  }
+    useEffect(() => {
+        if (visits.length > 0 && !loading) {
+            redirect("/mytrip");
+        }
+    }, [visits, loading]);
 
-  return <main className="h-dvh w-screen">{currentStep && currentStep}</main>;
+    if (currentStepIndex === 0) {
+        return (
+            <div>
+                {loading && (
+                    <div className="flex justify-center items-center w-screen h-dvh">
+                        <AnimationIcon
+                            duration={0.35}
+                            className="w-10 h-10"
+                            onComplete={() => {
+                                setLoading(false);
+                            }}
+                        />
+                    </div>
+                )}
+                <main className={`${loading ? "invisible" : ""}`}>
+                    <div className="w-screen h-dvh">{currentStep}</div>
+                    <div className="flex flex-col gap-4 p-6 py-12">
+                        <Heading type="h2">
+                            Neuer Konfigurator für Salzburg Card Erlebnisse
+                        </Heading>
+                        <Body>
+                            Verpasse keine Erlebnisse und entdecke die besten
+                            Angebote! Mit dem neuen Konfigurator kannst du deine
+                            Reise ganz nach deinen Wünschen gestalten.
+                        </Body>
+                        <Body>
+                            Sammle Inspiration, sichere dir exklusive Angebote
+                            und plane dein Abenteuer in Ruhe. Jeder Klick bringt
+                            dich deinem unvergesslichen Erlebnis näher.
+                        </Body>
+                    </div>
+                </main>
+            </div>
+        );
+    }
+
+    return <main className="w-screen h-dvh">{currentStep && currentStep}</main>;
 };
 
 export default Page;
