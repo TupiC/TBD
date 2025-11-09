0ca62b376ab5d2579d04f97d7b1b9352f59c6d0e
araz0 <alhamdani.araz@gmail.com>
2025-11-09 02:24:23 +0100

    feat: update styling for the Mytrip page header and Tabs component

--- apps/web/src/app/mytrip/page.tsx
+++ apps/web/src/app/mytrip/page.tsx
@@ -34,13 +34,17 @@ const Page = (): React.JSX.Element => {
       <h1
         className="text-white"
         style={{
-          fontFamily: "var(--font-geist-sans)",
+          fontSize: "32px",
+          marginBottom: "16px",
         }}
       >
         Your Trip
       </h1>
       <Tabs defaultValue="timeline" className="w-[100%] flex-1 flex flex-col">
-        <TabsList className="w-full flex gap-2 bg-gray-200 p-2 rounded-md">
+        <TabsList
+          className="w-full flex gap-2 bg-gray-200 p-2 rounded-md "
+          style={{ marginBottom: "16px" }}
+        >
           <TabsTrigger value="timeline" className="flex-1">
             Timeline
           </TabsTrigger>
